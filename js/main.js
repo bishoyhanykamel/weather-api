26303a50ae7116c796a69416e88192034ed27dec
Bishoy Hany <bishoyhany31@hotmail.com>
2024-01-17 16:29:31 +0200

    Add navbar for medium+ screens

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// API KEY: f40f32f309994030b4d121222241701
+// example query: http://api.weatherapi.com/v1/search.json?key=f40f32f309994030b4d121222241701&q=lon
+'use strict'
+const BASE_API_URL = 'http://api.weatherapi.com/v1';
+const API_KEY = 'f40f32f309994030b4d121222241701';
+
